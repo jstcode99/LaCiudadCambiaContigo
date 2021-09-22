8f7aebb6bbffb30eef49cb0b863dee5aec06a5bc
jstcode99 <jstorres0211@gmail.com>
2021-09-22 10:21:50 -0500

    Buscador y paginacion, mejoras en el menu y el el footer se anexo bootstrap

--- js/index.js
+++ js/index.js
@@ -4,12 +4,29 @@ var recordsPerPage = 8;
 var cuter = 4;
 
 function getData() {
-  $.getJSON('data.json',function(response){
-    data = response
-    changePage(1);
-  }).error(function(error){
-    console.log('error', error);
-  });
+  var queryURL = "data.json";
+  fetch(queryURL)
+    .then(function (response) {
+        // response.json() returns a json string,
+        // returning it will convert it 
+        // to a pure JavaScript 
+        // object for the next then's callback
+        return response.json();
+    })
+    .then(function (response) {
+        // users is a JavaScript object here
+        chargeDataCards(response)
+        autocomplete(document.getElementById("search"), response);
+    })
+    .catch(function (error) {
+        console.log('Error during fetch: ' + error.message);
+    });
+}
+
+function chargeDataCards(newData) {
+  currentPage = 1
+  data = newData
+  changePage(1);
 }
 
 function prevPage(){
@@ -28,45 +45,45 @@ function nextPage(){
 
 function appendRow(div) {
   var div = document.createElement("div");
-  div.classList.add('container');
+  div.classList.add('container-cards');
   return div
   // Append the new elements
 }
 
 function appendCard(data) {
   var divCard = document.createElement("div");
-  var classNames = ['card'];
+  var classNames = ['card-app'];
   classNames.forEach(className => {
     divCard.classList.add(className);
   });
 
   var divCardItem = document.createElement("div");
-  divCardItem.classList.add('card-item');
+  divCardItem.classList.add('card-app-item');
 
   var divCardImg = document.createElement("div");
-  divCardImg.classList.add('card-image');
+  divCardImg.classList.add('card-app-image');
   divCardImg.style.backgroundImage = "url(__URL__)".replace('__URL__', data.url);
   divCardItem.append(divCardImg);
 
 
 
   var divCardInfo = document.createElement("div");
-  var classNames2 = ['card-info'];
+  var classNames2 = ['card-app-info'];
   classNames2.forEach(className => {
     divCardInfo.classList.add(className);
   });
   var title = document.createElement("h2");
-  title.classList.add('card-title');
+  title.classList.add('card-app-title');
   title.innerText = data.title;
   divCardInfo.append(title);
 
   var category = document.createElement("small");
-  category.classList.add('card-category');
+  category.classList.add('card-app-category');
   category.innerText = data.idCategory;
   divCardInfo.append(category);
 
   var decription = document.createElement("p");
-  title.classList.add('card-title');
+  title.classList.add('card-app-title');
   decription.innerText = data.description;
   divCardInfo.append(decription);
 
@@ -78,7 +95,7 @@ function appendCard(data) {
 }
 
 function changePage(page){
-  document.getElementById('currentPage').textContent = currentPage + '/' + data.length
+  document.getElementById('currentPage').textContent = currentPage + '/' + numPages()
   var btnNext = document.getElementById("btnNext");
   var btnPrev = document.getElementById("btnPrev");
   var listingContent = document.getElementById("list-content");
@@ -119,6 +136,123 @@ function changePage(page){
 function numPages(){
   return Math.ceil(data.length / recordsPerPage);
 }
-$( document ).ready(function() {
-  getData();
-})
+
+function filterList(filter) {
+  const newData = data.filter(item => {
+    return item.title === filter;
+  });
+  chargeDataCards(newData)
+}
+
+function reset(event) {
+  var keyCode = event.which || event.keyCode;
+  console.log(keyCode, );
+  if (keyCode == 8) {
+    setTimeout(function(){
+      getData()
+      console.log("I am the third log after 5 seconds");
+    },2000);
+  }
+}
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + " autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].title.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].title.substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].title.substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i].title + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              let value = this.getElementsByTagName("input")[0].value;
+              inp.value = value;
+              filterList(value)
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+getData();
